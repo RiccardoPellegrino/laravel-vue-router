a1958193a719ea1c7c13cb1b5151ec62ee1ac238
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-18 15:56:57 +0100

    milestone 3 and 4

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppHeader></AppHeader>
   <div>
     <h1>{{titolo }}</h1>
     <ul>
@@ -7,47 +8,48 @@
       </li>
     </ul>
   </div>
+  <main>
+    <route-view>
+    </route-view>
+  </main>
 </template>
 
 <script>
 import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
 export default {
-  data() {
-    return {
-      projects: [],
-      urlApi:'http://localhost:8000/api',
-      titolo:'Bel titolo'
-    }
-  },
+  name:'App',
+    data() {
+        return {
+            projects: [],
+            urlApi: "http://localhost:8000/api",
+            titolo: "Bel titolo"
+        };
+    },
     methods: {
-      getProjects(){
-        axios.get(`${this.urlApi}/projects`).then((response)=>{
-          console.log(response.data);
-        this.projects = response.data.results.data;
-        })
-      }
-
-  },
-  computed: {
-
-  },
-
-  created() {
-    
-  },
-  mounted() {
-    this.getProjects();
-  },
-  beforeCreated() {
-    // console.log("beforeCreated")
-  },
-  beforeUpdate() {
-    // console.log("beforeUpdate")
-  },
-  update() {
-    // console.log("Update")
-  }
-
+        getProjects() {
+            axios.get(`${this.urlApi}/projects`).then((response) => {
+                console.log(response.data);
+                this.projects = response.data.results.data;
+            });
+        }
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+        this.getProjects();
+    },
+    beforeCreated() {
+        // console.log("beforeCreated")
+    },
+    beforeUpdate() {
+        // console.log("beforeUpdate")
+    },
+    update() {
+        // console.log("Update")
+    },
+    components: { AppHeader }
 }
 
 </script>
