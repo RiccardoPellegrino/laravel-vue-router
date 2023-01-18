11e4f8cd33258d8912de024a231fe94138c80402
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-18 16:34:38 +0100

    projectlist view

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,20 @@
 <template>
   <AppHeader></AppHeader>
-  <div>
-    <h1>{{titolo }}</h1>
-    <ul>
-      <li v-for="(project, index) in projects" :key="index">
-        {{ project.slug }}
-      </li>
-    </ul>
-  </div>
-  <main>
-    <route-view>
-    </route-view>
+  <main class="container">
+    <router-view></router-view>
   </main>
+  
 </template>
-
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
-export default {
-  name:'App',
-    data() {
-        return {
-            projects: [],
-            urlApi: "http://localhost:8000/api",
-            titolo: "Bel titolo"
-        };
-    },
-    methods: {
-        getProjects() {
-            axios.get(`${this.urlApi}/projects`).then((response) => {
-                console.log(response.data);
-                this.projects = response.data.results.data;
-            });
-        }
-    },
-    computed: {},
-    created() {
-    },
-    mounted() {
-        this.getProjects();
-    },
-    beforeCreated() {
-        // console.log("beforeCreated")
-    },
-    beforeUpdate() {
-        // console.log("beforeUpdate")
-    },
-    update() {
-        // console.log("Update")
-    },
-    components: { AppHeader }
-}
-
+  export default {
+    name: 'App',
+    components:{
+      AppHeader
+    }
+    
+  }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
