45be10521d0e21a9ea93325eead1274c859b7193
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-19 16:29:36 +0100

    mt-3 card projectlist

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -4,7 +4,7 @@
     <div class="row">
             <div class="col-12 col-md-4" v-for="(project, index) in projects" :key="index">
                 <div class="card" style="width: 18rem;">
-                    <img :src="`${store.imagBasePath}${project.cover_image}`" class="card-img-top" :alt="project.title" v-if="(project.cover_image)">
+                    <img :src="`${store.imagBasePath}${project.cover_image}`" class="card-img-top mt-3" :alt="project.title" v-if="(project.cover_image)">
                     <img src="https://via.placeholder.com/100x100.png?text=Placeholder" class="card-img-top" :alt="project.title" v-else>
                     <div class="card-body">
                         <h5 class="card-title">{{project.title}}</h5>
