ca9c93af866787167546ff050d59592c290f1214
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-20 17:56:12 +0100

    bozza front-end

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,12 +1,14 @@
 <template>
 
 
-    <div>
-        <h1>Il mio futuro come programmatore</h1>
-        <h2>Sono entusiasta di iniziare il mio viaggio come programmatore. Sono grato all'Accademia Boolean per avermi
+    <div class="container mt-3">
+        <h1>About me</h1>
+        <div class="mt-5 border 2px">
+        <h2 >Sono entusiasta di iniziare il mio viaggio come programmatore. Sono grato all'Accademia Boolean per avermi
             fornito le conoscenze e le competenze necessarie per avere successo in questo campo.</h2>
         <h2>Guardando al futuro, vedo infinite possibilità. Non vedo l'ora di applicare le mie competenze per creare
             software innovativi e di impatto. Sono fiducioso che le conoscenze e le esperienze acquisite costituiranno una solida base per il mio futuro come programmatore.</h2>
+        </div>
     </div>
 
 </template>
